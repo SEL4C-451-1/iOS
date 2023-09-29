3dedf286667dc9b003b97981bd46ec2a3d6014f5
EstherRC <a01656060@tec.mx>
2023-09-29 01:01:07 -0600

    Avance de actividades y corrección de errores

---
+++ SEL4C/SEL4C/Views/Activity1Views/Activity1_3EntryViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Activity1_3EntryViewController.swift
+//  SEL4C
+//
+//  Created by Esther Ramírez on 29/09/23.
+//
+
+import UIKit
+
+class Activity1_3EntryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
